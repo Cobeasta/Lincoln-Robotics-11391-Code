a57ff155a095d2d09f75016780e86715518299dc
Cobeasta <stopyloknok@gmail.com>
2017-12-09 10:43:06 -0600

    Teleop Superclass

---
+++ Users/Coby/Downloads/PLEASE_WORK/ftc_app-beta/TeamCode_reformatted/src/main/java/org/firstinspires/ftc/TeamCode_reformatted/TeleOp.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.TeamCode_reformatted;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Coby on 12/9/2017.
+ */
+
+public class TeleOp extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+    public void tankDrive(){
+
+    }
+    public void tankPOV(){
+
+    }
+
+}
