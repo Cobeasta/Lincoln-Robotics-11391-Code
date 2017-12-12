43294ae0e1da681f92869ad053c4e9d15037c09c
Cobeasta <stopyloknok@gmail.com>
2017-12-11 18:07:34 -0600

    Team code reformatted directory complete

---
+++ Users/Coby/Downloads/PLEASE_WORK/ftc_app-beta/TeamCode_reformatted/src/main/java/org/firstinspires/ftc/TeamCode_reformatted/commands/Wait.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.TeamCode_reformatted.commands;
+
+import org.firstinspires.ftc.TeamCode_reformatted.BasicCommand;
+import org.firstinspires.ftc.TeamCode_reformatted.Hardware;
+
+import java.util.Date;
+
+/**
+ * Created by Coby on 12/4/2017.
+ */
+
+/**
+ * Waits the specified number of milliseconds
+ * uses EPOCH to determine how much time has passed.
+ *
+ */
+public class Wait implements BasicCommand {
+
+    Date date;
+    private boolean finished;
+    long startTime;
+    long duration;
+
+    public Wait(long time){
+        duration = time;
+    }
+    @Override
+    public void init() {
+        date = new Date();
+        startTime = date.getTime();
+
+    }
+
+    @Override
+    public void execute(Hardware robot) {
+        date = new Date();
+        finished = date.getTime() - startTime >= duration;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finished;
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
