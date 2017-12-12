6db0f7e462dd1469d6d456d8af8fc5032ea34b60
Cobeasta <stopyloknok@gmail.com>
2017-12-11 19:34:04 -0600

    Cleaned up users

--- Users/Coby/Downloads/PLEASE_WORK/ftc_app-beta/TeamCode_reformatted/src/main/java/org/firstinspires/ftc/TeamCode_reformatted/BasicPID.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.TeamCode_reformatted;
-
-/**
- * Created by Coby on 12/11/2017.
- */
-
-public class BasicPID {
-}
--- Users/Coby/Downloads/PLEASE_WORK/ftc_app-beta/TeamCode_reformatted/src/main/java/org/firstinspires/ftc/TeamCode_reformatted/commands/UseLiftByTime.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.TeamCode_reformatted.commands;
-
-/**
- * Created by Coby on 12/11/2017.
- */
-
-public class UseLiftByTime {
-}
